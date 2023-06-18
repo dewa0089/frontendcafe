4e888db0d65917cbefcf87b4344f4c26ffab5e0d
dewa0089 <newdewa03@gmail.com>
2023-06-18 20:34:38 +0700

    comit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="home">
+    <Navbar />
+    <div class="container">
+      <Hero />
+
+      <div class="row mt-4">
+        <div class="col">
+          <h2>
+            Best
+            <strong>Foods</strong>
+          </h2>
+        </div>
+        <div class="col">
+          <router-link to="/foods" class="btn btn-success float-right">
+            <b-icon-eye></b-icon-eye>Lihat Semua
+          </router-link>
+        </div>
+      </div>
+
+      <div class="row mb-4">
+        <div class="col-md-4 mt-4" v-for="product in products" :key="product._id">
+          <CardProduct :product="product"/>
+         
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from "@/components/Navbar.vue";
+import Hero from "@/components/Hero.vue";
+import CardProduct from "@/components/CardProduct.vue"
+import axios from "axios";
+
+export default {
+  name: "HomeCafe",
+  components: {
+    Navbar,
+    Hero,
+    CardProduct
+  },
+  data() {
+    return {
+      products: [],
+    };
+  },
+  methods: {
+    setProducts(data) {
+      this.products = data;
+    },
+  },
+  mounted() {
+    axios
+      .get("https://testapi-pink.vercel.app/products/12/b")
+      .then((response) => this.setProducts(response.data))
+      .catch((error) => console.log(error))
+},
+};
+
+</script>
